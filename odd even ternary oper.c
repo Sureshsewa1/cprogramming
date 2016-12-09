dbec1092e571a1866276b172cb730e268f7be3bb
Sureshsewa1 <suresh.sewa2017@gmail.com>
2016-12-09 10:50:01 +0545

    Add files via upload

---
+++ odd even ternary oper.c
@@ -0,0 +1,23 @@
+/*Student Name: Suresh Sewa
+Subject: Programming Fundamental
+Roll No:
+Lab Sheet:
+Program: C program to print the largest number among three using if elseif ladder
+Date: 09/12/2016
+*/
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a;
+
+                printf("enter the first number:\n");
+                    scanf("%d",&a);
+    (a%2==0)?printf("Even \n"):printf("Odd \n");
+
+
+    getch();
+    return 0;
+
+
+}
