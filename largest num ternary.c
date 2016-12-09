9977538dd4549de459a17ffe9c602d50d6447a36
Sureshsewa1 <suresh.sewa2017@gmail.com>
2016-12-09 11:04:50 +0545

    Add files via upload

---
+++ largest num ternary.c
@@ -0,0 +1,27 @@
+/*Student Name: Suresh Sewa
+Subject: Programming Fundamental
+Roll No:
+Lab Sheet:
+Program: C program to print the largest number among two using if elseif ladder
+Date: 09/12/2016
+*/
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a,b;
+
+                printf("enter the first number:\n");
+    scanf("%d",&a);
+
+                printf("enter the second number:\n");
+    scanf("%d",&b);
+
+    (a-b>0)?printf("the largest number %d:\n",a):printf("the largest number %d:\n",b);
+
+
+    getch();
+    return 0;
+
+
+}
